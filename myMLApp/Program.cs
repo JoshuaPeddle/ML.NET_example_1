da057d4516abe1be93a7337cf11c838feb7a6477
Joshua Peddle <Joshuapeddle128@gmail.com>
2023-07-09 17:36:35 -0230

    Add multiple input samples

--- myMLApp/Program.cs
+++ myMLApp/Program.cs
@@ -1,13 +1,21 @@
 ﻿using MyMLApp;
-// Add input data
-var sampleData = new SentimentModel.ModelInput()
+using System.Collections.Generic;
+
+// Add multiple input data
+var samples = new List<SentimentModel.ModelInput>()
 {
-Col0 = "This restaurant was horrible."
+    new SentimentModel.ModelInput() { Col0 = "This restaurant was horrible." },
+    new SentimentModel.ModelInput() { Col0 = "Wow, great meal!" },
+    new SentimentModel.ModelInput() { Col0 = "The service was terrible." },
+    // Add more samples here as needed
 };
 
-// Load model and predict output of sample data
-var result = SentimentModel.Predict(sampleData);
+// Load model and predict output for each sample data
+foreach (var sample in samples)
+{
+    var result = SentimentModel.Predict(sample);
 
-// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
-var sentiment = result.Prediction == 1 ? "Positive" : "Negative";
-Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+    // If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+    var sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+    Console.WriteLine($"Text: {sample.Col0}\nSentiment: {sentiment}\n");
+}
